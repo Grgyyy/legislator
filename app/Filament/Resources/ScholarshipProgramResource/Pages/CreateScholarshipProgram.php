5383a4f1762c3cb1b64c801b3bfad5499cb621db
mrkiyn <amado.markian@gmail.com>
2024-07-19 11:13:13 +0800

    feat: Create of Scholarship Programs Model

---
+++ app/Filament/Resources/ScholarshipProgramResource/Pages/CreateScholarshipProgram.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ScholarshipProgramResource\Pages;
+
+use App\Filament\Resources\ScholarshipProgramResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateScholarshipProgram extends CreateRecord
+{
+    protected static string $resource = ScholarshipProgramResource::class;
+}
