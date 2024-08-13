6d67179ccddb7e8a4acd9fd2ca6a56eaa9410fc4
TJ Maurea Dhanniela <tmdlojerio@tesda.gov.ph>
2024-08-13 15:57:05 +0800

    feat: add active and inactive tabs in legislator

--- app/Filament/Resources/LegislatorResource/Pages/ListLegislators.php
+++ app/Filament/Resources/LegislatorResource/Pages/ListLegislators.php
@@ -4,10 +4,14 @@ namespace App\Filament\Resources\LegislatorResource\Pages;
 
 use App\Filament\Resources\LegislatorResource;
 use App\Imports\LegislatorImport;
+use App\Models\Legislator;
 use Filament\Actions;
 use Filament\Actions\Action;
 use Filament\Forms\Components\FileUpload;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\CreateAction;
+use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ListLegislators extends ListRecords
@@ -29,6 +33,27 @@ class ListLegislators extends ListRecords
 
                     Excel::import(new LegislatorImport, $file);
                 })
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+            'Active' => Tab::make()
+                ->modifyQueryUsing(function ($query) {
+                    $query->where('status', 'Active');
+                })
+                ->badge(function () {
+                    return Legislator::where('status', 'Active')->count();
+                }),
+            'Inactive' => Tab::make()
+                ->modifyQueryUsing(function ($query) {
+                    $query->where('status', 'Inactive');
+                })
+                ->badge(function () {
+                    return Legislator::where('status', 'Inactive')->count();
+                }),
         ];
     }
 }
