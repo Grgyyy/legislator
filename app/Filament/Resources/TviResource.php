dc407e6f2696566f1ae036203a84ee7eb95acad6
Cedric James Leala <cleala4@gmail.com>
2024-08-15 14:48:03 +0800

    Feat: Fix Exporter

--- app/Filament/Resources/TviResource.php
+++ app/Filament/Resources/TviResource.php
@@ -113,7 +113,9 @@ class TviResource extends Resource
                     Tables\Actions\RestoreBulkAction::make(),
                     ExportBulkAction::make()
                         ->exporter(TVIExporter::class)
+
                 ]),
+
             ]);
     }
 
