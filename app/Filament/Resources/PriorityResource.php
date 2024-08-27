5d3f8e781e68524bc453d0dfa30f48b16739fbd0
iyanayi <mimamado@tesda.gov.ph>
2024-08-27 13:29:12 +0800

    feat: Created top ten priorities sector Table and Resource

---
+++ app/Filament/Resources/PriorityResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PriorityResource\Pages;
+use App\Filament\Resources\PriorityResource\RelationManagers;
+use App\Models\Priority;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PriorityResource extends Resource
+{
+    protected static ?string $model = Priority::class;
+
+    protected static ?string $navigationLabel = "Ten Priority Sectors";
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Sector Name')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPriorities::route('/'),
+            'create' => Pages\CreatePriority::route('/create'),
+            'edit' => Pages\EditPriority::route('/{record}/edit'),
+        ];
+    }
+}
