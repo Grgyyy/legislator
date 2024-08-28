4759a194ca1f40528f709527d268f0f9a0cc6b55
iyanayi <mimamado@tesda.gov.ph>
2024-08-28 17:23:23 +0800

    fix: Adjust Qualification Database Migration

---
+++ database/migrations/2024_08_28_051421_create_qualification_titles_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('qualification_titles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('training_program_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            // $table->foreignId('abdd_id')
+            //     ->default(1)
+            //     ->constrained()
+            //     ->cascadeOnDelete();
+            $table->foreignId('scholarship_program_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('tvet_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('priority_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->integer('duration')->default(0);
+            $table->decimal('training_cost_pcc', 10, 2)->default(0);
+            $table->decimal('cost_of_toolkit_pcc', 10, 2)->default(0);
+            $table->decimal('training_support_fund', 10, 2)->default(0);
+            $table->decimal('assessment_fee', 10, 2)->default(0);
+            $table->decimal('entrepeneurship_fee', 10, 2)->default(0);
+            $table->decimal('new_normal_assisstance', 10, 2)->default(0);
+            $table->decimal('accident_insurance', 10, 2)->default(0);
+            $table->decimal('book_allowance', 10, 2)->default(0);
+            $table->foreignId('status_id')
+                ->default(1)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('qualification_titles');
+    }
+};
