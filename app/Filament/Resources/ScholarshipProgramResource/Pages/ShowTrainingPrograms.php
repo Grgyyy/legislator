85e4a2d193b37256470b547a50366bc91b3df6c7
iyanayi <mimamado@tesda.gov.ph>
2024-08-30 13:20:23 +0800

    fix: Fixed Navigation of the Scholarship Program and Training Program

---
+++ app/Filament/Resources/ScholarshipProgramResource/Pages/ShowTrainingPrograms.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\ScholarshipProgramResource\Pages;
+
+use App\Filament\Resources\TrainingProgramResource;
+use App\Models\ScholarshipProgram;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Actions;
+use Illuminate\Database\Eloquent\Builder;
+
+class ShowTrainingPrograms extends ListRecords
+{
+    protected static string $resource = TrainingProgramResource::class;
+
+    protected ?string $heading = 'Training Programs';
+
+    public function getBreadcrumbs(): array
+    {
+        $scholarshipId = $this->getScholarshipProgramId();
+        
+        $scholarship = ScholarshipProgram::find($scholarshipId);
+
+        $scholarshipProgramId = $scholarship->id;
+
+        return [
+            route('filament.admin.resources.scholarship-programs.showTrainingPrograms', ['record' => $scholarshipProgramId]) => $scholarship->name ?? 'Scholarship Programs',
+            'Training Programs',
+            'List'
+        ];
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->icon('heroicon-m-plus')
+                ->label('New')
+        ];
+    }
+
+    protected function getScholarshipProgramId(): ?int
+    {
+        return (int) request()->route('record');
+    }
+}
+
