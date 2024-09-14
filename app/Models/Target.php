33293710ca5ae04956865464b63ef7f8dd037027
iyanayi <mimamado@tesda.gov.ph>
2024-09-14 12:44:43 +0800

    fix: Resolve computation of the target total costings

--- app/Models/Target.php
+++ app/Models/Target.php
@@ -11,15 +11,26 @@ class Target extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
+        'allocation_type',
         'allocation_id',
         'tvi_id',
-        'priority_id',
-        'tvet_id',
         'abdd_id',
         'qualification_title_id',
         'number_of_slots',
+
+        'total_training_cost_pcc',
+        'total_cost_of_toolkit_pcc',
+        'total_training_support_fund',
+        'total_assessment_fee',
+        'total_entrepeneurship_fee',
+        'total_new_normal_assisstance',
+        'total_accident_insurance',
+        'total_book_allowance',
+        'total_uniform_allowance',
+        'total_misc_fee',
         'total_amount',
-        'status_id',
+        'appropriation_type',
+        'target_status_id'
     ];
 
     public function allocation()
@@ -52,7 +63,7 @@ class Target extends Model
         return $this->belongsTo(QualificationTitle::class);
     }
 
-    public function status()
+    public function targetStatus()
     {
         return $this->belongsTo(Status::class);
     }
