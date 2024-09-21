c0330c35e56f553b1e7aa9d2d07bf75974cefd89
iyanayi <mimamado@tesda.gov.ph>
2024-09-21 11:29:08 +0800

    feat: Add Partylist Import Action

--- app/Filament/Resources/PartylistResource/Pages/ListPartylists.php
+++ app/Filament/Resources/PartylistResource/Pages/ListPartylists.php
@@ -3,9 +3,14 @@
 namespace App\Filament\Resources\PartylistResource\Pages;
 
 use App\Filament\Resources\PartylistResource;
-use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
+use Exception;
+use Maatwebsite\Excel\Facades\Excel;
+use Filament\Notifications\Notification;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use App\Imports\PartylistImport;
 
 class ListPartylists extends ListRecords
 {
@@ -27,6 +32,29 @@ class ListPartylists extends ListRecords
             CreateAction::make()
                 ->icon('heroicon-m-plus')
                 ->label('New'),
+            ACtion::make('RegionImport')
+                ->label('Import')
+                ->icon('heroicon-o-document-arrow-up')
+                ->form([
+                    FileUpload::make('attachment'),
+                ])
+                ->action(function (array $data) {
+                    $file = public_path('storage/' . $data['attachment']);
+                    try {
+                        Excel::import(new PartylistImport, $file);
+                        Notification::make()
+                            ->title('Import Successful')
+                            ->body('Region import successful!')
+                            ->success()
+                            ->send();
+                    } catch (Exception $e) {
+                        Notification::make()
+                            ->title('Import Failed')
+                            ->body('Partylist import failed: ' . $e->getMessage())
+                            ->danger()
+                            ->send();
+                    }
+                }),
         ];
     }
 }
