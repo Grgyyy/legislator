12ad6c8527d90132f97a8efaee3bdd7e9adb73b8
iyanayi <mimamado@tesda.gov.ph>
2024-09-30 10:17:08 +0800

    refactor: Remove Target Comments Navigation Button from Sidebar

--- app/Filament/Resources/TargetCommentResource.php
+++ app/Filament/Resources/TargetCommentResource.php
@@ -24,6 +24,9 @@ class TargetCommentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function form(Form $form): Form
     {
         $targetIdParams = request()->query('record');
