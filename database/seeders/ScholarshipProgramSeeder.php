ab8752d4bc75ae43341087affab8ef214583fd76
iyanayi <mimamado@tesda.gov.ph>
2024-10-17 15:45:36 +0800

    fix: Resolve search issue on the allocation resource

--- database/seeders/ScholarshipProgramSeeder.php
+++ database/seeders/ScholarshipProgramSeeder.php
@@ -15,6 +15,7 @@ class ScholarshipProgramSeeder extends Seeder
     {
         $programs = [
             ['code' => 'TWSP', 'name' => 'TWSP', 'desc' => 'Training for Work Scholarship'],
+            ['code' => 'TWSP', 'name' => 'TWSP-PAFSE', 'desc' => 'Training for Work Scholarship - Program on Accelerating Farm School Establishment'],
             ['code' => 'STEP', 'name' => 'STEP', 'desc' => 'Special Training for Employment Program'],
             ['code' => 'TTSP', 'name' => 'TTSP', 'desc' => 'Tulong Trabaho Scholarship Program'],
             ['code' => 'CFSP', 'name' => 'CFSP', 'desc' => 'Coconut Farmers Scholarships Program'],
