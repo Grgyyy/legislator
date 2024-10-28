ec7fa70372ab348c8cfa66dbc4e2cf69d5730f77
iyanayi <mimamado@tesda.gov.ph>
2024-10-28 23:05:49 +0800

    chore: Updated create action for pending targets to include saving of attribution records

--- app/Filament/Resources/TargetResource/Pages/CreateTarget.php
+++ app/Filament/Resources/TargetResource/Pages/CreateTarget.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\TargetResource\Pages;
 use App\Filament\Resources\TargetResource;
 use App\Models\Allocation;
 use App\Models\Legislator;
+use App\Models\Particular;
 use App\Models\QualificationTitle;
 use App\Models\Target;
 use App\Models\TargetHistory;
@@ -55,7 +56,16 @@ class CreateTarget extends CreateRecord
                 }
             }
 
-            $attributedLegislator = Legislator::where('id', $targetData['legislator_id'])->first();
+            $attributedLegislator = Legislator::find($targetData['legislator_id']);
+            $attributedLegislatorParticular = Particular::find($targetData['attribution_particular_id']);
+
+            $attributedAllocation = $attributedLegislator && $attributedLegislatorParticular
+                ? Allocation::where('legislator_id', $attributedLegislator->id)
+                    ->where('particular_id', $attributedLegislatorParticular->id)
+                    ->where('scholarship_program_id', $targetData['scholarship_program_id'])
+                    ->where('year', $targetData['allocation_year'])
+                    ->first()
+                : null;
 
             $allocation = Allocation::where('legislator_id', $targetData['allocation_legislator_id'])
                 ->where('particular_id', $targetData['particular_id'])
@@ -64,7 +74,7 @@ class CreateTarget extends CreateRecord
                 ->first();
 
             if (!$allocation) {
-                throw new \Exception('Allocation not found.');
+                throw new \Exception('Primary Allocation not found.');
             }
 
             $qualificationTitle = QualificationTitle::find($targetData['qualification_title_id']);
@@ -74,21 +84,9 @@ class CreateTarget extends CreateRecord
 
             $numberOfSlots = $targetData['number_of_slots'] ?? 0;
 
-            $total_training_cost_pcc = $qualificationTitle->training_cost_pcc * $numberOfSlots;
-            $total_cost_of_toolkit_pcc = $qualificationTitle->cost_of_toolkit_pcc * $numberOfSlots;
-            $total_training_support_fund = $qualificationTitle->training_support_fund * $numberOfSlots;
-            $total_assessment_fee = $qualificationTitle->assessment_fee * $numberOfSlots;
-            $total_entrepreneurship_fee = $qualificationTitle->entrepreneurship_fee * $numberOfSlots;
-            $total_new_normal_assisstance = $qualificationTitle->new_normal_assisstance * $numberOfSlots;
-            $total_accident_insurance = $qualificationTitle->accident_insurance * $numberOfSlots;
-            $total_book_allowance = $qualificationTitle->book_allowance * $numberOfSlots;
-            $total_uniform_allowance = $qualificationTitle->uniform_allowance * $numberOfSlots;
-            $total_misc_fee = $qualificationTitle->misc_fee * $numberOfSlots;
             $total_amount = $qualificationTitle->pcc * $numberOfSlots;
 
-            // Check allocation balance
             if ($allocation->balance >= $total_amount) {
-                // Create target
                 $target = Target::create([
                     'legislator_id' => $attributedLegislator->id ?? null,
                     'allocation_id' => $allocation->id,
@@ -96,27 +94,26 @@ class CreateTarget extends CreateRecord
                     'qualification_title_id' => $qualificationTitle->id,
                     'abdd_id' => $targetData['abdd_id'],
                     'number_of_slots' => $numberOfSlots,
-                    'total_training_cost_pcc' => $total_training_cost_pcc,
-                    'total_cost_of_toolkit_pcc' => $total_cost_of_toolkit_pcc,
-                    'total_training_support_fund' => $total_training_support_fund,
-                    'total_assessment_fee' => $total_assessment_fee,
-                    'total_entrepreneurship_fee' => $total_entrepreneurship_fee,
-                    'total_new_normal_assisstance' => $total_new_normal_assisstance,
-                    'total_accident_insurance' => $total_accident_insurance,
-                    'total_book_allowance' => $total_book_allowance,
-                    'total_uniform_allowance' => $total_uniform_allowance,
-                    'total_misc_fee' => $total_misc_fee,
                     'total_amount' => $total_amount,
                     'appropriation_type' => $targetData['appropriation_type'],
                     'target_status_id' => 1,
                 ]);
 
-                $allocation->balance -= $total_amount;
+                if ($attributedLegislator) {
+                    if ($attributedAllocation) {
+                        $attributedAllocation->balance -= $total_amount;
+                        $attributedAllocation->attribution_sent += $total_amount;
+                        $attributedAllocation->save();
+                    }
+                } else {
+                    $allocation->balance -= $total_amount;
+                    $allocation->save();
+                }
 
                 if ($attributedLegislator) {
-                    $allocation->attribution_received = $total_amount;
+                    $allocation->attribution_received += $total_amount;
+                    $allocation->save();
                 }
-                $allocation->save();
 
                 TargetHistory::create([
                     'target_id' => $target->id,
@@ -125,16 +122,6 @@ class CreateTarget extends CreateRecord
                     'qualification_title_id' => $qualificationTitle->id,
                     'abdd_id' => $targetData['abdd_id'],
                     'number_of_slots' => $numberOfSlots,
-                    'total_training_cost_pcc' => $total_training_cost_pcc,
-                    'total_cost_of_toolkit_pcc' => $total_cost_of_toolkit_pcc,
-                    'total_training_support_fund' => $total_training_support_fund,
-                    'total_assessment_fee' => $total_assessment_fee,
-                    'total_entrepreneurship_fee' => $total_entrepreneurship_fee,
-                    'total_new_normal_assisstance' => $total_new_normal_assisstance,
-                    'total_accident_insurance' => $total_accident_insurance,
-                    'total_book_allowance' => $total_book_allowance,
-                    'total_uniform_allowance' => $total_uniform_allowance,
-                    'total_misc_fee' => $total_misc_fee,
                     'total_amount' => $total_amount,
                     'appropriation_type' => $targetData['appropriation_type'],
                     'description' => 'Target Created',
