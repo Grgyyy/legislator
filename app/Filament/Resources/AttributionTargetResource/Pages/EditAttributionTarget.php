fbae746fd3dd065e53260d635917d5626e2cb388
iyanayi <mimamado@tesda.gov.ph>
2024-11-07 13:42:41 +0800

    feat: Fixed Edit Action of the Attribution

--- app/Filament/Resources/AttributionTargetResource/Pages/EditAttributionTarget.php
+++ app/Filament/Resources/AttributionTargetResource/Pages/EditAttributionTarget.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\AttributionTargetResource\Pages;
 
 use App\Filament\Resources\AttributionTargetResource;
 use App\Models\Allocation;
+use App\Models\QualificationTitle;
+use App\Models\TargetHistory;
 use DB;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -22,6 +24,11 @@ class EditAttributionTarget extends EditRecord
     }
 
     protected ?string $heading = 'Edit Attribution Target';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function mutateFormDataBeforeFill(array $data): array
     {
@@ -61,6 +68,82 @@ class EditAttributionTarget extends EditRecord
 
             if (!$receiverAllocation) {
                 throw new \Exception('Attribution Sender Allocation not found');
+            }
+
+            $qualificationTitle = QualificationTitle::find($data['qualification_title_id']);
+
+            if (!$qualificationTitle) {
+                throw new \Exception('Qualification Title not found');
+            }
+
+            $numberOfSlots = $data['number_of_slots'] ?? 0;
+            $total_amount = $qualificationTitle->pcc * $numberOfSlots;
+
+            $senderAllocation->balance += $record['total_amount'];
+            $senderAllocation->attribution_sent -= $record['total_amount'];
+            $senderAllocation->save();
+
+            $receiverAllocation->attribution_received -= $record['total_amount'];
+            $receiverAllocation->save();
+
+            if ($senderAllocation->balance >= $total_amount) { 
+                $senderAllocation->balance -= $total_amount;
+                $senderAllocation->attribution_sent += $total_amount;
+                $senderAllocation->save();
+
+                $receiverAllocation->attribution_received += $total_amount;
+                $receiverAllocation->save();
+
+                TargetHistory::create([
+                    'target_id' => $record->id,
+                    'allocation_id' => $senderAllocation->id,
+                    'attribution_allocation_id' => $receiverAllocation->id,
+                    'tvi_id' => $data['tvi_id'],
+                    'qualification_title_id' => $data['qualification_title_id'],
+                    'abdd_id' => $data['abdd_id'],
+                    'number_of_slots' => $data['number_of_slots'],
+                    'total_training_cost_pcc' => $qualificationTitle->training_cost_pcc * $numberOfSlots,
+                    'total_cost_of_toolkit_pcc' => $qualificationTitle->cost_of_toolkit_pcc * $numberOfSlots,
+                    'total_training_support_fund' => $qualificationTitle->training_support_fund * $numberOfSlots,
+                    'total_assessment_fee' => $qualificationTitle->assessment_fee * $numberOfSlots,
+                    'total_entrepreneurship_fee' => $qualificationTitle->entrepreneurship_fee * $numberOfSlots,
+                    'total_new_normal_assisstance' => $qualificationTitle->new_normal_assisstance * $numberOfSlots,
+                    'total_accident_insurance' => $qualificationTitle->accident_insurance * $numberOfSlots,
+                    'total_book_allowance' => $qualificationTitle->book_allowance * $numberOfSlots,
+                    'total_uniform_allowance' => $qualificationTitle->uniform_allowance * $numberOfSlots,
+                    'total_misc_fee' => $qualificationTitle->misc_fee * $numberOfSlots,
+                    'total_amount' => $total_amount,
+                    'appropriation_type' => $data['attribution_appropriation_type'],
+                    'target_status_id' => 1,
+                    'description' => 'Target Edited',
+                ]);
+
+                $record->update([
+                    'allocation_id' => $senderAllocation->id,
+                    'attribution_allocation_id' => $receiverAllocation->id,
+                    'tvi_id' => $data['tvi_id'],
+                    'qualification_title_id' => $data['qualification_title_id'],
+                    'abdd_id' => $data['abdd_id'],
+                    'number_of_slots' => $data['number_of_slots'],
+                    'total_training_cost_pcc' => $qualificationTitle->training_cost_pcc * $numberOfSlots,
+                    'total_cost_of_toolkit_pcc' => $qualificationTitle->cost_of_toolkit_pcc * $numberOfSlots,
+                    'total_training_support_fund' => $qualificationTitle->training_support_fund * $numberOfSlots,
+                    'total_assessment_fee' => $qualificationTitle->assessment_fee * $numberOfSlots,
+                    'total_entrepreneurship_fee' => $qualificationTitle->entrepreneurship_fee * $numberOfSlots,
+                    'total_new_normal_assisstance' => $qualificationTitle->new_normal_assisstance * $numberOfSlots,
+                    'total_accident_insurance' => $qualificationTitle->accident_insurance * $numberOfSlots,
+                    'total_book_allowance' => $qualificationTitle->book_allowance * $numberOfSlots,
+                    'total_uniform_allowance' => $qualificationTitle->uniform_allowance * $numberOfSlots,
+                    'total_misc_fee' => $qualificationTitle->misc_fee * $numberOfSlots,
+                    'total_amount' => $total_amount,
+                    'appropriation_type' => $data['attribution_appropriation_type'],
+                    'target_status_id' => 1,
+                ]);
+                
+                return $record;
+            }
+            else {
+                throw new \Exception('Insufficient balance for allocation');
             }
         });
     }
