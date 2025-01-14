d428d0a744ac105d9d3c1299d7cb0c786f878bd8
iyanayi <mimamado@tesda.gov.ph>
2025-01-14 22:39:52 +0800

    modify: Adjust Institution Import (sample with delete temp file function)

--- app/Filament/Resources/TviResource/Pages/ListTvis.php
+++ app/Filament/Resources/TviResource/Pages/ListTvis.php
@@ -11,6 +11,7 @@ use Filament\Actions\CreateAction;
 use Filament\Forms\Components\FileUpload;
 use Maatwebsite\Excel\Facades\Excel;
 use Exception;
+use Illuminate\Support\Facades\Storage;
 
 class ListTvis extends ListRecords
 {
@@ -41,13 +42,18 @@ class ListTvis extends ListRecords
                         ->required(),
                 ])
                 ->action(function (array $data) {
-                    $file = public_path('storage/' . $data['attachment']);
+                    $filePath = public_path('storage/' . $data['attachment']);
 
                     try {
-                        Excel::import(new TviImport, $file);
+                        Excel::import(new TviImport, $filePath);
                         NotificationHandler::sendSuccessNotification('Import Successful', 'The institution training programs have been successfully imported from the file.');
+
                     } catch (Exception $e) {
-                        NotificationHandler::sendErrorNotification('Import Failed', 'There was an issue importing the institution training programs : ' . $e->getMessage());
+                        NotificationHandler::sendErrorNotification('Import Failed', 'There was an issue importing the institution training programs: ' . $e->getMessage());
+                    } finally {
+                        if (file_exists($filePath)) {
+                            unlink($filePath);
+                        }
                     }
                 }),
         ];
