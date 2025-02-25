ba8977419ebc731a3e84e2563aea2f468813e785
cdrc1280 <cleala4@gmail.com>
2025-02-25 14:06:52 +0800

    chore: Modified the export functionality for ScholarshipProgramExport and added a logo for improved presentation.

--- app/Exports/ScholarshipProgramExport.php
+++ app/Exports/ScholarshipProgramExport.php
@@ -5,14 +5,18 @@ namespace App\Exports;
 use App\Models\ScholarshipProgram;
 use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithDrawings;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ScholarshipProgramExport implements FromQuery, WithMapping, WithStyles, WithHeadings
+class ScholarshipProgramExport implements FromQuery, WithMapping, WithStyles, WithHeadings, WithDrawings
 {
     private array $columns = [
         'code' => 'Code',
@@ -47,6 +51,21 @@ class ScholarshipProgramExport implements FromQuery, WithMapping, WithStyles, Wi
         return array_merge($customHeadings, [array_values($this->columns)]);
     }
 
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('TESDA Logo');
+        $drawing->setDescription('TESDA Logo');
+        $drawing->setPath(public_path('images/TESDA_logo.png'));
+        $drawing->setHeight(90);
+        $drawing->setCoordinates('B1');
+        $drawing->setOffsetX(0);
+        $drawing->setOffsetY(1);
+
+        return $drawing;
+    }
+
+
     public function styles(Worksheet $sheet)
     {
         $columnCount = count($this->columns);
@@ -78,7 +97,18 @@ class ScholarshipProgramExport implements FromQuery, WithMapping, WithStyles, Wi
                 'horizontal' => Alignment::HORIZONTAL_CENTER,
                 'vertical' => Alignment::VERTICAL_CENTER,
             ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '7a8078'],
+                ],
+            ],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['argb' => 'D3D3D3'],
+            ],
         ];
+
 
         $sheet->getStyle("A1:A3")->applyFromArray($headerStyle);
         $sheet->getStyle("A4:{$lastColumn}4")->applyFromArray($subHeaderStyle);
@@ -89,6 +119,30 @@ class ScholarshipProgramExport implements FromQuery, WithMapping, WithStyles, Wi
             $sheet->getColumnDimension($columnLetter)->setAutoSize(true);
         }
 
-        return $sheet;
+        $dynamicBorderStyle = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'],
+                ],
+            ],
+        ];
+
+        $row = 6;
+        while (true) {
+            $hasData = false;
+            foreach (range(1, $columnCount) as $colIndex) {
+                $columnLetter = Coordinate::stringFromColumnIndex($colIndex);
+                if ($sheet->getCell("{$columnLetter}{$row}")->getValue() !== null) {
+                    $hasData = true;
+                    break;
+                }
+            }
+            if (!$hasData) {
+                break;
+            }
+            $sheet->getStyle("A{$row}:{$lastColumn}{$row}")->applyFromArray($dynamicBorderStyle);
+            $row++;
+        }
     }
 }
