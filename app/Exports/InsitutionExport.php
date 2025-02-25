9f6268bc25c55d2dcf58ee4a0c8743777c9d197f
cdrc1280 <cleala4@gmail.com>
2025-02-25 14:23:20 +0800

    chore: Modified the export functionality for InstitutionExport and added a logo for improved presentation.

--- app/Exports/InsitutionExport.php
+++ app/Exports/InsitutionExport.php
@@ -5,14 +5,18 @@ namespace App\Exports;
 use App\Models\Tvi;
 use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithDrawings;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class InsitutionExport implements FromQuery, WithHeadings, WithStyles, WithMapping
+class InsitutionExport implements FromQuery, WithHeadings, WithStyles, WithMapping, WithDrawings
 {
     private $columns = [
         'school_id' => 'School ID',
@@ -58,6 +62,38 @@ class InsitutionExport implements FromQuery, WithHeadings, WithStyles, WithMappi
 
     }
 
+    private function formatCurrency($amount)
+    {
+        // Use the NumberFormatter class to format the currency in the Filipino Peso (PHP)
+        $formatter = new \NumberFormatter('en_PH', \NumberFormatter::CURRENCY);
+        return $formatter->formatCurrency($amount, 'PHP');
+    }
+
+    private function getQualificationTitle($record)
+    {
+        $qualificationTitles = $record->qualificationTitles->map(
+            fn($qualificationTitle) =>
+            optional($qualificationTitle->trainingProgram)->soc_code .
+            ' - ' .
+            optional($qualificationTitle->trainingProgram)->title
+        )->filter()->toArray();
+
+        return empty($qualificationTitles) ? '-' : implode(', ', $qualificationTitles);
+    }
+
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('TESDA Logo');
+        $drawing->setDescription('TESDA Logo');
+        $drawing->setPath(public_path('images/TESDA_logo.png'));
+        $drawing->setHeight(90);
+        $drawing->setCoordinates('C1');
+        $drawing->setOffsetX(50);
+        $drawing->setOffsetY(0);
+
+        return $drawing;
+    }
 
 
     public function styles(Worksheet $sheet)
@@ -91,7 +127,18 @@ class InsitutionExport implements FromQuery, WithHeadings, WithStyles, WithMappi
                 'horizontal' => Alignment::HORIZONTAL_CENTER,
                 'vertical' => Alignment::VERTICAL_CENTER,
             ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '7a8078'],
+                ],
+            ],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['argb' => 'D3D3D3'],
+            ],
         ];
+
 
         $sheet->getStyle("A1:A3")->applyFromArray($headerStyle);
         $sheet->getStyle("A4:{$lastColumn}4")->applyFromArray($subHeaderStyle);
@@ -102,25 +149,30 @@ class InsitutionExport implements FromQuery, WithHeadings, WithStyles, WithMappi
             $sheet->getColumnDimension($columnLetter)->setAutoSize(true);
         }
 
-        return $sheet;
-    }
+        $dynamicBorderStyle = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'],
+                ],
+            ],
+        ];
 
-    private function formatCurrency($amount)
-    {
-        // Use the NumberFormatter class to format the currency in the Filipino Peso (PHP)
-        $formatter = new \NumberFormatter('en_PH', \NumberFormatter::CURRENCY);
-        return $formatter->formatCurrency($amount, 'PHP');
-    }
-
-    private function getQualificationTitle($record)
-    {
-        $qualificationTitles = $record->qualificationTitles->map(
-            fn($qualificationTitle) =>
-            optional($qualificationTitle->trainingProgram)->soc_code .
-            ' - ' .
-            optional($qualificationTitle->trainingProgram)->title
-        )->filter()->toArray();
-
-        return empty($qualificationTitles) ? '-' : implode(', ', $qualificationTitles);
+        $row = 6;
+        while (true) {
+            $hasData = false;
+            foreach (range(1, $columnCount) as $colIndex) {
+                $columnLetter = Coordinate::stringFromColumnIndex($colIndex);
+                if ($sheet->getCell("{$columnLetter}{$row}")->getValue() !== null) {
+                    $hasData = true;
+                    break;
+                }
+            }
+            if (!$hasData) {
+                break;
+            }
+            $sheet->getStyle("A{$row}:{$lastColumn}{$row}")->applyFromArray($dynamicBorderStyle);
+            $row++;
+        }
     }
 }
