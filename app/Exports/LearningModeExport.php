e7efe2ed710f4867b50832d2548295f0859d0e06
cdrc1280 <cleala4@gmail.com>
2025-02-25 14:47:02 +0800

    chore: Modified the export functionality for LearningModeExport and added a logo for improved presentation.

--- app/Exports/LearningModeExport.php
+++ app/Exports/LearningModeExport.php
@@ -5,14 +5,18 @@ namespace App\Exports;
 use App\Models\LearningMode;
 use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithDrawings;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class LearningModeExport implements FromQuery, WithHeadings, WithStyles, WithMapping
+class LearningModeExport implements FromQuery, WithHeadings, WithStyles, WithMapping, WithDrawings
 {
     private $columns = [
         'name' => 'Learning Mode',
@@ -43,6 +47,21 @@ class LearningModeExport implements FromQuery, WithHeadings, WithStyles, WithMap
             $record->name,
             $record->deliveryMode->pluck('acronym')->implode(', '),
         ];
+    }
+
+
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('TESDA Logo');
+        $drawing->setDescription('TESDA Logo');
+        $drawing->setPath(public_path('images/TESDA_logo.png'));
+        $drawing->setHeight(90);
+        $drawing->setCoordinates('A1');
+        $drawing->setOffsetX(0);
+        $drawing->setOffsetY(0);
+
+        return $drawing;
     }
 
 
@@ -77,7 +96,18 @@ class LearningModeExport implements FromQuery, WithHeadings, WithStyles, WithMap
                 'horizontal' => Alignment::HORIZONTAL_CENTER,
                 'vertical' => Alignment::VERTICAL_CENTER,
             ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '7a8078'],
+                ],
+            ],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['argb' => 'D3D3D3'],
+            ],
         ];
+
 
         $sheet->getStyle("A1:A3")->applyFromArray($headerStyle);
         $sheet->getStyle("A4:{$lastColumn}4")->applyFromArray($subHeaderStyle);
@@ -88,6 +118,30 @@ class LearningModeExport implements FromQuery, WithHeadings, WithStyles, WithMap
             $sheet->getColumnDimension($columnLetter)->setAutoSize(true);
         }
 
-        return $sheet;
+        $dynamicBorderStyle = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'],
+                ],
+            ],
+        ];
+
+        $row = 6;
+        while (true) {
+            $hasData = false;
+            foreach (range(1, $columnCount) as $colIndex) {
+                $columnLetter = Coordinate::stringFromColumnIndex($colIndex);
+                if ($sheet->getCell("{$columnLetter}{$row}")->getValue() !== null) {
+                    $hasData = true;
+                    break;
+                }
+            }
+            if (!$hasData) {
+                break;
+            }
+            $sheet->getStyle("A{$row}:{$lastColumn}{$row}")->applyFromArray($dynamicBorderStyle);
+            $row++;
+        }
     }
 }
