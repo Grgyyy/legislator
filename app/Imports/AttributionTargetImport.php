f6c0cacd9faa84322f8264af7f644ac555f3cbbf
iyanayi <mimamado@tesda.gov.ph>
2025-03-06 09:54:40 +0800

    enhance: add additional validation to Target and Project Proposal import files

--- app/Imports/AttributionTargetImport.php
+++ app/Imports/AttributionTargetImport.php
@@ -89,6 +89,8 @@ class AttributionTargetImport implements ToModel, WithHeadingRow
                 $qualificationTitle = $this->getQualificationTitle($row['qualification_title'], $row['soc_code'], $row['qualification_title_scholarship_program'], $scholarship_program);
                 $totals = $this->calculateTotals($qualificationTitle, $numberOfSlots);
 
+                $this->validateInstitutionProgram($tvi->id, $qualificationTitle->trainingProgram->id);
+
                 $skillPriority = $this->getSkillPriority(
                     $qualificationTitle->training_program_id,
                     $tvi->district_id,
@@ -193,6 +195,27 @@ class AttributionTargetImport implements ToModel, WithHeadingRow
         $pastYear = $currentYear - 1;
         if ($year != $currentYear && $year != $pastYear) {
             throw new Exception("The provided year '{$year}' must be either the current year '{$currentYear}' or the previous year '{$pastYear}'.");
+        }
+    }
+
+    protected function validateInstitutionProgram(int $tviId, int $qualiId) {
+        $institution = Tvi::find($tviId);
+        $quali = TrainingProgram::find($qualiId);
+    
+        if (!$institution) {
+            throw new Exception("Institution with ID {$tviId} not found.");
+        }
+    
+        if (!$quali) {
+            throw new Exception("Qualification with ID {$qualiId} not found.");
+        }
+        
+        $instiPrograms = $institution->trainingPrograms()->pluck('training_programs.id');
+
+        if (!$instiPrograms->contains($qualiId)) {
+            throw new Exception(
+                "The qualification title '{$quali->title}' is not registered under the institution '{$institution->name}'."
+            );
         }
     }
 
